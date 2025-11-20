3d2397098cf56f20d705da3ffc56a037e7d802ba
Bhavesh <bhavesh.zode.phe22@itbhu.ac.in>
2025-11-20 22:56:15 +0530

    github data added

--- index.js
+++ index.js
@@ -6,6 +6,42 @@ const app = express()
 
 const port = 4000
 
+const githubData = {
+  "login": "BhaveshZode",
+  "id": 172618872,
+  "node_id": "U_kgDOCkn0eA",
+  "avatar_url": "https://avatars.githubusercontent.com/u/172618872?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/BhaveshZode",
+  "html_url": "https://github.com/BhaveshZode",
+  "followers_url": "https://api.github.com/users/BhaveshZode/followers",
+  "following_url": "https://api.github.com/users/BhaveshZode/following{/other_user}",
+  "gists_url": "https://api.github.com/users/BhaveshZode/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/BhaveshZode/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/BhaveshZode/subscriptions",
+  "organizations_url": "https://api.github.com/users/BhaveshZode/orgs",
+  "repos_url": "https://api.github.com/users/BhaveshZode/repos",
+  "events_url": "https://api.github.com/users/BhaveshZode/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/BhaveshZode/received_events",
+  "type": "User",
+  "user_view_type": "public",
+  "site_admin": false,
+  "name": null,
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 8,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 0,
+  "created_at": "2024-06-13T15:07:29Z",
+  "updated_at": "2025-10-30T09:59:54Z"
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -20,6 +56,10 @@ app.get('/login',(req,res)=>{
 
 app.get('/youtube',(req,res)=>{
     res.send("<h2>Chai aur Code</h2>")
+})
+
+app.get('/github',(req,res)=>{
+    res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
